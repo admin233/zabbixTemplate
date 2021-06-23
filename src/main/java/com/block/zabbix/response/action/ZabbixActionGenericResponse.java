173eb2644c6c536b21d92a3823a778845ba5ecb9
admin233 <cprogramfiles@tom.com>
2021-06-23 11:24:39 +0800

    【add】Action Generic Response

---
+++ src/main/java/com/block/zabbix/response/action/ZabbixActionGenericResponse.java
@@ -0,0 +1,16 @@
+package com.block.zabbix.response.action;
+
+import java.util.List;
+
+public class ZabbixActionGenericResponse {
+
+    private List<String> actionids;
+
+    public List<String> getActionids() {
+        return actionids;
+    }
+
+    public void setActionids(List<String> actionids) {
+        this.actionids = actionids;
+    }
+}
